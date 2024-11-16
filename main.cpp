60442db2276402ed643ea28b125f7c803654c610
Juwon-14 <wndnjs9127@gmail.com>
2024-11-15 21:08:32 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 //15/9/2024
 
 #include <iostream>
-
 using namespace std;
 
 int main() {
@@ -11,19 +10,15 @@ int main() {
     int value2; 
     int value3; 
 
-    // Ask the user for first value and second value
+    // Ask the user for the first value and second value
     cout << "Enter the first value: ";
     cin >> value1;
 
     cout << "Enter the second value: ";
     cin >> value2;
 
-    // Determine the larger value using an if statement
-    if (value1 > value2) {
-        value3 = value1;
-    } else {
-        value3 = value2;
-    }
+    // Determine the larger value using the conditional operator
+    value3 = (value1 > value2) ? value1 : value2;
 
     // Output the larger value
     cout << "The larger value is: " << value3 << endl;
